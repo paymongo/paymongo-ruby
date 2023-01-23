94e4e0f9b3cb9d624217947d0efd40931858f503
traqy-paymongo <traqy@paymongo.com>
2023-01-23 16:15:57 +0800

    Alphabetize customer entity attributes

--- lib/paymongo/entities/customer.rb
+++ lib/paymongo/entities/customer.rb
@@ -4,6 +4,7 @@ module Paymongo
       attr_reader :id,
                   :default_device,
                   :default_payment_method_id,
+                  :deleted,
                   :email,
                   :first_name,
                   :last_name,
@@ -11,13 +12,13 @@ module Paymongo
                   :phone,
                   :type,
                   :created_at,
-                  :deleted,
                   :updated_at
 
       def initialize(api_resource)
         @id = api_resource.id
         @default_device = api_resource.attributes['default_device']
         @default_payment_method_id = api_resource.attributes['default_payment_method_id']
+        @deleted = api_resource.attributes['deleted']
         @email = api_resource.attributes['email']
         @first_name = api_resource.attributes['first_name']
         @last_name = api_resource.attributes['last_name']
@@ -25,7 +26,6 @@ module Paymongo
         @phone = api_resource.attributes['phone']
         @type = api_resource.data['type']
         @created_at = api_resource.attributes['created_at']
-        @deleted = api_resource.attributes['deleted']
         @updated_at = api_resource.attributes['updated_at']
       end
     end
