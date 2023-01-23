fbda0a7305bea2b1dedbf34a51628e584b7b9374
traqy-paymongo <traqy@paymongo.com>
2023-01-23 15:58:22 +0800

    Remove commas

--- lib/paymongo/entities/customer.rb
+++ lib/paymongo/entities/customer.rb
@@ -15,15 +15,15 @@ module Paymongo
                   :updated_at
 
       def initialize(api_resource)
-        @id = api_resource.id,
-        @default_device = api_resource.attributes['default_device'],
-        @default_payment_method_id = api_resource.attributes['default_payment_method_id'],
-        @email = api_resource.attributes['email'],
-        @first_name = api_resource.attributes['first_name'],
-        @last_name = api_resource.attributes['last_name'],
-        @livemode = api_resource.attributes['livemode'],
-        @phone = api_resource.attributes['phone'],
-        @type = api_resource.data['type'],
+        @id = api_resource.id
+        @default_device = api_resource.attributes['default_device']
+        @default_payment_method_id = api_resource.attributes['default_payment_method_id']
+        @email = api_resource.attributes['email']
+        @first_name = api_resource.attributes['first_name']
+        @last_name = api_resource.attributes['last_name']
+        @livemode = api_resource.attributes['livemode']
+        @phone = api_resource.attributes['phone']
+        @type = api_resource.data['type']
         @created_at = api_resource.attributes['created_at']
         @deleted = api_resource.attributes['deleted']
         @updated_at = api_resource.attributes['updated_at']
